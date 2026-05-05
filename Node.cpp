d79c83735e52fa450e64c652b1512f6869d71471
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-05-05 13:52:40 +0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,11 +1,11 @@
-﻿#include "Node.h"
+#include "Node.h"
 
 namespace containers {
 
     template<typename T>
     Node<T>::Node(const T& value) : data(value), next(nullptr), prev(nullptr) {}
 
-    // Явная инстанциация для типа int
+
     template class Node<int>;
 
 }
